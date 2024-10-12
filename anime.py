45bd5f55de80645243e141a119560a6f3ec34387
NSIX06 <felipebugalho2016@gmail.com>
2024-10-12 19:40:47 -0400

    100%

--- anime.py
+++ anime.py
@@ -6,30 +6,6 @@ with open("jojo.json", encoding='utf-8') as meu__json:
 def line():
     print("-----------------------------------")
 
-"""
+
 print(jojo)
-"""
 
-#*chaves
-for c in jojo:
-    print(c)
-line()
-
-#*itens
-for i in jojo.items():
-    print(i)
-line()
-
-#*valores
-for v in jojo.values():
-    print(v)
-line()
-
-#*nome do autor
-print(jojo["autor"])
-line()
-
-#*detalhes
-for d in jojo["detalhes"].values():
-    print(d)
-line()
